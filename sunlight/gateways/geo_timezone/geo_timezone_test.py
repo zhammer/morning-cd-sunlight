e6f39f21cd869b1d7f05d6dd204413eb3a054f7b
zhammer <zachary_hammer@alumni.brown.edu>
2019-01-05 10:16:17 -0500

    Fix linter issue

--- sunlight/gateways/geo_timezone/geo_timezone_test.py
+++ sunlight/gateways/geo_timezone/geo_timezone_test.py
@@ -3,4 +3,5 @@ from .geo_timezone import GeoTimezoneGateway
 
 def test_can_ignore_extra_iana_prefix() -> None:
     gateway = GeoTimezoneGateway()
-    assert gateway.fetch_timezone_coordinates('America/Indiana/Indianapolis') == gateway.fetch_timezone_coordinates('America/Indianapolis')
+    assert (gateway.fetch_timezone_coordinates('America/Indiana/Indianapolis') ==
+            gateway.fetch_timezone_coordinates('America/Indianapolis'))

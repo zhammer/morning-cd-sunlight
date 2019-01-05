deed92382eeebba1298afddd4a70582e4be7196d
zhammer <zachary_hammer@alumni.brown.edu>
2019-01-05 10:35:05 -0500

    Test that we can fetch montreal

--- sunlight/gateways/geo_timezone/geo_timezone_test.py
+++ sunlight/gateways/geo_timezone/geo_timezone_test.py
@@ -5,3 +5,8 @@ def test_can_ignore_extra_iana_prefix() -> None:
     gateway = GeoTimezoneGateway()
     assert (gateway.fetch_timezone_coordinates('America/Indiana/Indianapolis') ==
             gateway.fetch_timezone_coordinates('America/Indianapolis'))
+
+
+def test_can_fetch_montreal() -> None:
+    gateway = GeoTimezoneGateway()
+    gateway.fetch_timezone_coordinates('America/Montreal')

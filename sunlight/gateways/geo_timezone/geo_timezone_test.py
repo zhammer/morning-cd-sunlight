02d7060d0811b9645a44a0ecb128d000eb54e0a7
zhammer <zachary_hammer@alumni.brown.edu>
2019-01-08 00:01:29 -0500

    Add test for asia/calcutta backwards compatibility

--- sunlight/gateways/geo_timezone/geo_timezone_test.py
+++ sunlight/gateways/geo_timezone/geo_timezone_test.py
@@ -10,3 +10,9 @@ def test_can_ignore_extra_iana_prefix() -> None:
 def test_can_fetch_montreal() -> None:
     gateway = GeoTimezoneGateway()
     gateway.fetch_timezone_coordinates('America/Montreal')
+
+
+def test_can_fetch_kolkata_as_calcutta() -> None:
+    gateway = GeoTimezoneGateway()
+    assert (gateway.fetch_timezone_coordinates('Asia/Kolkata') ==
+            gateway.fetch_timezone_coordinates('Asia/Calcutta'))
